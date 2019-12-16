813b7ccd362c16c8fda7aabcbed06febfec8aeae
Tony <mr.tonyhe@gmail.com>
2019-12-16 16:56:01 +1100

    nscc rename
    nscc rename

---
+++ RenameNSCC.java
@@ -0,0 +1,93 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RenameNSCC {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		// Path of folder where files are located 
+        String folder_path = 
+               "/Users/mba/Downloads/nscc"; 
+  
+        // creating new folder 
+        File myfolder = new File(folder_path); 
+  
+        File[] file_array = myfolder.listFiles(); 
+        for (int i = 0; i < file_array.length; i++) 
+        { 
+  
+            if (file_array[i].isFile()) 
+            { 
+  
+            	
+                //File myfile = new File(folder_path + 
+                //         "\\" + file_array[i].getName());
+                
+                File myfile = file_array[i];
+                
+                String fileName = myfile.getName();
+                if (fileName.contains("12-Dec-19")) {
+                	
+                	String newFileName = folder_path + "/" + fileName.replace("12-Dec-19", "13-Dec-19");
+                	System.out.println("newFileName=" + newFileName);
+                	//myfile.renameTo(new File(newFileName));
+                }
+                
+                /*
+                System.out.println("Before Format : " + myfile.lastModified());
+            	
+            	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+            		
+            	System.out.println("After Format : " + sdf.format(myfile.lastModified()));
+                
+                System.out.println(convertTime(myfile.lastModified()));
+                
+                System.out.println(myfile.getName() + " creation time :"
+                        + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss")
+                                   .format(getCreationTime(myfile).toMillis()));
+                
+                String long_file_name = file_array[i].getName(); 
+                
+                String[] tokens = long_file_name.split("\\s"); 
+                String new_file_name = tokens[1]; 
+                System.out.println(long_file_name); 
+                System.out.print(new_file_name); 
+  
+                // file name format: "Snapshot 11 (12-05-2017 11-57).png" 
+                // To Shorten it to "11.png", get the substring which 
+                // starts after the first space character in the long 
+                // _file_name. 
+                myfile.renameTo(new File(folder_path + 
+                             "\\" + new_file_name + ".png")); 
+                             
+                             */
+            } 
+        } 
+    } 
+
+	public static String convertTime(long time){
+	    Date date = new Date(time);
+	    Format format = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
+	    return format.format(date);
+	}
+	
+	public static FileTime getCreationTime(File file) throws IOException {
+	    Path p = Paths.get(file.getAbsolutePath());
+	    BasicFileAttributes view
+	        = Files.getFileAttributeView(p, BasicFileAttributeView.class)
+	                    .readAttributes();
+	    FileTime fileTime=view.creationTime();
+	    //  also available view.lastAccessTine and view.lastModifiedTime
+	    return fileTime;
+	  }
+
+}
